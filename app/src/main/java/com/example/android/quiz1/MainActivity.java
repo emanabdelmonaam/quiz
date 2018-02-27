07749ae2f8b4481187d9dd0329f4334591642642
emanabdelmonaam <emanabdelmonaam@gmail.com>
2018-02-27 22:05:16 +0200

    third project

--- app/src/main/java/com/example/android/quiz1/MainActivity.java
+++ app/src/main/java/com/example/android/quiz1/MainActivity.java
@@ -33,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // resultNumberSubmited = false;
-        //resultNumberSubmited = true;
         score = 0;
         again(resultNumberSubmited);
     }
